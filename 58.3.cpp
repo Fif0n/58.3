499063a4ee5253986f4b6efed167f37ce179bc26
Fif0n <48093214+Fif0n@users.noreply.github.com>
2020-10-28 19:52:06 +0100

    Add files via upload

--- 58.3.cpp
+++ 58.3.cpp
@@ -3,53 +3,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 using namespace std;
 
-int records = 1;
+int main(int argc, char** argv)
+{
+ ifstream file1, file2, file3;
+ ofstream fileOut;
+ char* endptr;
+ long number1, number2, number3, max1, max2, max3;
+ string hour1, hour2, hour3, num1, num2, num3;
+ file1.open("c:\\dane_systemy1.txt");
+ file2.open("c:\\dane_systemy2.txt");
+ file3.open("c:\\dane_systemy3.txt");
+ bool first = true;
+ int record = 1;
+     while(!file1.eof())
+     {
+               file1>>hour1 >> num1;
+               file2>>hour2 >> num2;
+               file3>>hour3 >> num3;
+               number1 =  strtol(num1.c_str(), &endptr, 2);
+               number2 =  strtol(num2.c_str(), &endptr, 4);
+               number3 =  strtol(num3.c_str(), &endptr, 8);
+               if(first)
+               {
+                            max1 = number1;
+                            max2 = number2;
+                            max3 = number3;
+                            first = false;
+               }
+                  if(number1 > max1){
+                  	
+                  	max1 = number1;
+                  	record++;
+				  }else if(number2 > max2){
+				  	
+				  	max2 = number2;
+				  	record++;
+				  } else if(number3 > max3){
+				  	
+				  	max3 = number3;
+				  	record++;
+				  }
+                       
+     }
+ cout<<record;
+ file1.close();
+ file2.close();
+ file3.close();
 
-int days[100];
-
-long record(string fileName, int system){
-	long max, number;
-	int day, i = 0;
-	string num1, num2;
-	bool first = true;
-	char* endptr;
-	ifstream file;
-	ofstream fileOut;
-	file.open(fileName.c_str());
-	while(!file.eof()){
-		file>>num1>>num2;
-		day++;
-		number = strtol(num2.c_str(), &endptr, system);
-		if(first){
-			max = number;
-			first = false;
-		} else
-			if(number > max){
-				max = number;
-				records++;
-				days[i] = day;
-				i++;
-			}
-				
-	}
-	
-	return max;
-}
-
-int main(int argc, char** argv) {
-	for(int i = 0; i<=100; i++){
-		days[i] = 0;
-	}
-	record("c:\\dane_systemy1.txt", 2);
-	record("c:\\dane_systemy2.txt", 4);
-	record("c:\\dane_systemy3.txt", 8);
-//	cout<<day;
-	for(int i = 0; i<=100; i++){
-		cout<<days[i]<<endl;
-	}
-//	cout<<records;
-	
-	return 0;
+ return 0;
 }
